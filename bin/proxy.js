3fc8a9aa1db498a1547573a8a0cd54e767065288
Gajus Kuizinas <gk@anuary.com>
2015-02-18 14:42:46 +0000

    Code organisation.

--- bin/proxy.js
+++ bin/proxy.js
@@ -14,12 +14,12 @@ command = commander
     .action(function (env) {
         var program = Program(env),
             WebProxy = require('../src/web-proxy'),
+            DataStore = require('../src/data-store'),
+            dataStore,
             bunyan = require('bunyan'),
             logger = bunyan.createLogger({name: 'web-proxy'}),
             config = {},
-            dataStore = {},
-            server,
-            db;
+            server;
 
         config.logger = logger;
 
@@ -29,82 +29,23 @@ command = commander
                 dbDatabase: env.dbDatabase
             });
 
-            db = program.database();
+            dataStore = DataStore.database(program.database());
+        } else {
+            dataStore = DataStore.session();
         }
 
-        /**
-         * @param {Object} reference
-         * @param {String} reference.method
-         * @param {String} reference.url
-         * @return {Null} Returning null will allow HTTP request to progress.
-         * @return {Object} response
-         * @return {Number} response.statusCode
-         * @return {Object} response.headers
-         * @return {String} response.body
-         */
         config.read = function (request) {
-            var key;
-
             if (request.method !== 'get') {
                 logger.trace('Not logging non GET request.');
 
                 return;
             }
 
-            if (db) {
-                return db
-                    .query('SELECT `status_code`, `headers`, `body` FROM `request` WHERE `method` = ? AND `url` = ? ORDER BY `created_at` DESC LIMIT 1', [
-                        request.method,
-                        request.url
-                    ])
-                    .then(function (rows) {
-                        if (!rows.length) {
-                            return;
-                        }
-
-                        return {
-                            statusCode: rows[0].status_code,
-                            headers: JSON.parse(rows[0].headers),
-                            body: rows[0].body
-                        };
-                    });
-            } else {
-                key = JSON.stringify([request.method, request.url]);
-
-                if (dataStore[key]) {
-                    return dataStore[key];
-                }
-            }
-
-            return;
+            return dataStore.read(request);
         };
 
-        /**
-         * @param {Object} reference
-         * @param {String} reference.method
-         * @param {String} reference.url
-         * @param {Object} response
-         * @param {Number} response.statusCode
-         * @param {Object} response.headers
-         * @param {String} response.body
-         */
         config.write = function (request, response) {
-            var key;
-
-            if (db) {
-                return db
-                    .query('INSERT INTO `request` SET `method` = ?, `url` = ?, `status_code` = ?, `headers` = ?, `body` = ?', [
-                        request.method,
-                        request.url,
-                        response.statusCode,
-                        JSON.stringify(response.headers),
-                        response.body
-                    ]);
-            } else {
-                key = JSON.stringify([request.method, request.url]);
-
-                dataStore[key] = response;
-            }
+            return dataStore.write(request, response);
         };
 
         server = WebProxy(config);
